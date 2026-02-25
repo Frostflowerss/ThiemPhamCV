a87dd439a7b38c34cc6b86df5b039dbe5e1942f5
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-02-25 19:50:20 +0700

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -47,4 +47,28 @@ export const projects: Project[] = [
     highlights: ["Clash review support", "Coordination", "Schedule-driven output"],
     images: ["/projects/p3.jpg"],
   },
+  {
+    id: "Test xem sao",
+    title: "Trường quay VTV.S1 & Quảng trường trung tâm",
+    role: "BIM Coordinator",
+    period: "2023 – 2024",
+    location: "—",
+    classText: "Construction class: II",
+    description:
+      "Điều phối mô hình, đối soát xung đột và hỗ trợ team triển khai bản vẽ kỹ thuật theo tiến độ bàn giao.",
+    highlights: ["Clash review support", "Coordination", "Schedule-driven output"],
+    images: ["/projects/p4.jpg"],
+  },
+  {
+    id: "vádfádf",
+    title: "Trường quay VTV.S1 & Quảng trường trung tâm",
+    role: "BIM Coordinator",
+    period: "2023 – 2024",
+    location: "—",
+    classText: "Construction class: II",
+    description:
+      "Điều phối mô hình, đối soát xung đột và hỗ trợ team triển khai bản vẽ kỹ thuật theo tiến độ bàn giao.",
+    highlights: ["Clash review support", "Coordination", "Schedule-driven output"],
+    images: ["/projects/p5.jpg"],
+  },
 ];
