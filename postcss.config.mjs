fa746413b68f49b1ccfd8785fcaaea4aef3ebd62
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-08-08 15:08:28 +0700

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-import { getSiteData } from "@/lib/data";
-import { CvView } from "@/components/cv-view";
-
-export const dynamic = "force-dynamic";
-
-export default async function CvPage() {
-  const data = await getSiteData();
-  return <CvView data={data} />;
-}
