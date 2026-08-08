f4160c821902db430a3f098075c2a1218916c721
Frostflowerss <130375717+Frostflowerss@users.noreply.github.com>
2026-08-08 15:05:39 +0700

    Delete data.ts

--- data.ts
+++
@@ -1,9 +0,0 @@
-import { getSiteData } from "@/lib/data";
-import { HomeView } from "@/components/home-view";
-
-export const dynamic = "force-dynamic";
-
-export default async function HomePage() {
-  const data = await getSiteData();
-  return <HomeView data={data} />;
-}
